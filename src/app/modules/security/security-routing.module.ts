c314098a6dc41775de048845c3739b65de06f0a2
Jhonatan Toro Gomez <jhonatan.1701322237@ucaldas.edu.co>
2020-07-22 09:25:50 -0500

    AdicionModelosDeSecurity

--- src/app/modules/security/security-routing.module.ts
+++ src/app/modules/security/security-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent}from './login/login.component';
 import {LogoutComponent}from './logout/logout.component';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 const routes: Routes = [
   {
     path:'login',
@@ -10,6 +12,14 @@ const routes: Routes = [
   {
     path:'logout',
     component: LogoutComponent
+  },
+  {
+    path:'reset',
+    component: PasswordResetComponent
+  },
+  {
+    path:'change-password',
+    component: ChangePasswordComponent
   }
 ];
 
